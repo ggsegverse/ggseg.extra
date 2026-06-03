c501859930e283d0b087f22af26b45012678b02d
Oli <hackerman@xsmail.com>
2026-06-03 11:46:16 +0100

    Improve command construction in utils-snapshot.R
    Refactor command construction to check for FreeSurfer initialization.

--- R/utils-snapshot.R
+++ R/utils-snapshot.R
@@ -237,7 +237,12 @@ run_cmd <- function(cmd, verbose = get_verbose(), no_ui = FALSE) {
       cmd <- paste("fsxvfb", cmd)
     }
   }
-  full_cmd <- paste0(sub(";\\s*$", "", freesurfer::get_fs()), "; ", cmd)
+  fs_init <- sub(";\\s*$", "", freesurfer::get_fs())
+  full_cmd <- if (nzchar(trimws(fs_init))) {
+    paste0(fs_init, "; ", cmd)
+  } else {
+    cmd
+  }
   suppress <- verbose < 2
   exit_code <- system(
     paste("bash -c", shQuote(full_cmd)),
