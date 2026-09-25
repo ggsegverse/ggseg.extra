8a9513569119301b54c4f44e978d1aee8a9bc5ec
GitHub Action <action@github.com>
2026-09-25 15:08:20 +0000

    Update coverage badge

--- tests/testthat/_snaps/atlas_geometry.md
+++
@@ -1,9 +0,0 @@
-# extract_contours / logs progress when verbose is TRUE
-
-    Code
-      invisible(extract_contours(input_dir, output_dir, verbose = TRUE))
-    Message
-      i  Extracting contours
-      v  Extracting contours [<time>]
-      
-
--- tests/testthat/helper-snapshot.R
+++
@@ -1,6 +0,0 @@
-# Gap and vertex counts that come out of sf/GEOS geometry work can shift
-# between GEOS versions, so snapshots keep the message but not the number.
-scrub_geometry_counts <- function(lines) {
-  lines <- gsub("Filling [0-9]+ small", "Filling <n> small", lines)
-  gsub("has [0-9]+ vertices", "has <n> vertices", lines)
-}
